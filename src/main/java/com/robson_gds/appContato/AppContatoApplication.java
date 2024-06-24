d4b18f0ade50022f7d2ab23589c923147f4e5de3
Robson <robson.080490@gmail.com>
2024-06-24 12:42:17 -0300

    first commit

---
+++ src/main/java/com/robson_gds/appContato/AppContatoApplication.java
@@ -0,0 +1,13 @@
+package com.robson_gds.appContato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppContatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppContatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robson_gds/appContato/AppContatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robson_gds.appContato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppContatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
